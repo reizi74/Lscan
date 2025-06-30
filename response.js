904bd0e15da94d42619ea39ab3bc5495e4cfd8f0
reizi74 <reizi74.exe@mail.ru>
2025-06-30 13:35:11 +0700

    first commit

---
+++ response.js
@@ -0,0 +1,329 @@
+const _0x4ecc08 = _0x2e06;
+(function (_0x27feef, _0x7d84f3) {
+  const _0x3ea586 = _0x2e06,
+    _0x3e7fc9 = _0x27feef();
+  while (!![]) {
+    try {
+      const _0xa8ece8 =
+        parseInt(_0x3ea586(0x10f)) / 0x1 +
+        parseInt(_0x3ea586(0xf8)) / 0x2 +
+        -parseInt(_0x3ea586(0x12a)) / 0x3 +
+        (parseInt(_0x3ea586(0x10c)) / 0x4) *
+          (-parseInt(_0x3ea586(0x110)) / 0x5) +
+        -parseInt(_0x3ea586(0x12f)) / 0x6 +
+        parseInt(_0x3ea586(0x126)) / 0x7 +
+        parseInt(_0x3ea586(0x137)) / 0x8;
+      if (_0xa8ece8 === _0x7d84f3) break;
+      else _0x3e7fc9["push"](_0x3e7fc9["shift"]());
+    } catch (_0x421a36) {
+      _0x3e7fc9["push"](_0x3e7fc9["shift"]());
+    }
+  }
+})(_0x340b, 0x31c34);
+const mc = require(_0x4ecc08(0x11d));
+function _0x2e06(_0x487c2b, _0x484d2b) {
+  const _0x340b06 = _0x340b();
+  return (
+    (_0x2e06 = function (_0x2e0638, _0x419963) {
+      _0x2e0638 = _0x2e0638 - 0xf5;
+      let _0x10e5e3 = _0x340b06[_0x2e0638];
+      return _0x10e5e3;
+    }),
+    _0x2e06(_0x487c2b, _0x484d2b)
+  );
+}
+process[_0x4ecc08(0x12c)][_0x4ecc08(0x130)] < 0x6 &&
+  (console["log"](_0x4ecc08(0x139)), process[_0x4ecc08(0xfc)](0x1));
+let RECONNECT_WITH_BUNGEEHACK = ![];
+const host = process["argv"][0x2],
+  port = parseInt(process[_0x4ecc08(0x12c)][0x3]),
+  version = process[_0x4ecc08(0x12c)][0x4],
+  username = process[_0x4ecc08(0x12c)][0x5];
+function getTextFromJSON(_0x5841c6) {
+  const _0x2c488c = _0x4ecc08;
+  let _0x49e12e = null;
+  try {
+    _0x49e12e = JSON[_0x2c488c(0xfd)](_0x5841c6);
+  } catch (_0xb2e284) {
+    return _0x5841c6;
+  }
+  let _0x15390b = "";
+  function _0x49c84e(_0x28b491) {
+    const _0x241eb2 = _0x2c488c;
+    if (Array[_0x241eb2(0xf6)](_0x28b491))
+      _0x28b491["forEach"]((_0x434f87) => {
+        const _0x2eaae2 = _0x241eb2;
+        _0x434f87[_0x2eaae2(0x12b)] && (_0x15390b += _0x434f87["text"]),
+          _0x434f87["translate"] && (_0x15390b += _0x434f87[_0x2eaae2(0x123)]),
+          _0x434f87[_0x2eaae2(0x121)] && _0x49c84e(_0x434f87[_0x2eaae2(0x121)]),
+          _0x434f87[_0x2eaae2(0xfe)]("with") &&
+            (_0x15390b += getTextFromJSON(
+              JSON[_0x2eaae2(0x108)](_0x434f87[_0x2eaae2(0x111)])
+            ));
+      });
+    else {
+      if (_0x28b491[_0x241eb2(0x12b)]) _0x15390b = _0x28b491[_0x241eb2(0x12b)];
+      else _0x28b491[_0x241eb2(0x123)] && (_0x15390b = _0x28b491["translate"]);
+    }
+  }
+  if (Array[_0x2c488c(0xf6)](_0x49e12e["extra"]))
+    _0x49c84e(_0x49e12e[_0x2c488c(0x121)]);
+  else {
+    if (_0x49e12e[_0x2c488c(0x12b)]) _0x15390b = _0x49e12e["text"];
+    else
+      _0x49e12e[_0x2c488c(0x123)] && (_0x15390b = _0x49e12e[_0x2c488c(0x123)]);
+  }
+  if (_0x15390b[_0x2c488c(0x124)]() === "") return _0x5841c6;
+  return _0x15390b[_0x2c488c(0x124)]();
+}
+class BotResponse {
+  constructor(_0x5bc6d6, _0x436176 = "", _0x27c716 = "") {
+    const _0x50cdfd = _0x4ecc08;
+    (this["message"] = _0x5bc6d6),
+      (this[_0x50cdfd(0x120)] = _0x436176),
+      (this[_0x50cdfd(0xf9)] = _0x27c716);
+  }
+  [_0x4ecc08(0x112)]() {
+    const _0x52fbbe = _0x4ecc08;
+    console["log"](
+      this[_0x52fbbe(0x133)] +
+        "/#MCPTOOL#/" +
+        this[_0x52fbbe(0x120)] +
+        _0x52fbbe(0x113) +
+        this[_0x52fbbe(0xf9)]
+    ),
+      process["exit"](0x0);
+  }
+}
+function _0x340b() {
+  const _0xa7c905 = [
+    "connect",
+    "minecraft:register",
+    "Timeout\x20(1)",
+    "stringify",
+    "_setupEventListeners",
+    "fakeHost",
+    "_handleEnd",
+    "36orFTNU",
+    "data",
+    "host",
+    "378119ZoLBal",
+    "58810mzLmEL",
+    "with",
+    "showCliResponse",
+    "/#MCPTOOL#/",
+    "match",
+    "_handleDisconnect",
+    "If\x20you\x20wish\x20to\x20use\x20IP\x20forwarding,\x20please\x20enable\x20it\x20in\x20your\x20BungeeCord\x20config\x20as\x20well!",
+    "serverPluginsMessage",
+    "playerJoin",
+    "disconnect",
+    "\x20&cis\x20not\x20currently\x20supported.",
+    "login",
+    "&cIncompatible\x20Minecraft\x20version:\x20Version\x20&c&l",
+    "minecraft-protocol",
+    "minecraft:brand",
+    "&cTimeout\x20&7(&dConnected\x20with\x20bungeehack&7)",
+    "brand",
+    "extra",
+    "\x00127.0.0.1\x00bdf844e7-54f3-39c9-b577-b275f5753e80",
+    "translate",
+    "trim",
+    "you\x20are\x20using\x20version",
+    "1157618zFvjik",
+    "toString",
+    "username",
+    "pluginChannelsMessage",
+    "752076uYLziO",
+    "text",
+    "argv",
+    "client",
+    "createClient",
+    "1311180VWPPcC",
+    "length",
+    "_handleError",
+    "version",
+    "message",
+    "debug",
+    "_handleCustomPayload",
+    "bind",
+    "281200UHFfEg",
+    "utf-8",
+    "Usage:\x20node\x20response.js\x20<host>\x20<port>\x20<version>\x20<username>",
+    "replace",
+    "Error\x20message:",
+    "isArray",
+    "includes",
+    "400530OmVxCr",
+    "pluginChannels",
+    "&cTimeout",
+    "_handleLogin",
+    "exit",
+    "parse",
+    "hasOwnProperty",
+    "channel",
+    "port",
+    "Reconnecting\x20with\x20BungeeHack...",
+    "is\x20not\x20supported",
+    "log",
+    "brandMessage",
+  ];
+  _0x340b = function () {
+    return _0xa7c905;
+  };
+  return _0x340b();
+}
+class MinecraftClient {
+  constructor({
+    host: _0x5891e1,
+    port: _0x4e0919,
+    username: _0x41f627,
+    version: _0x18e227,
+    fakeHost: fakeHost = null,
+    debug: debug = ![],
+  }) {
+    const _0x848469 = _0x4ecc08;
+    (this["host"] = _0x5891e1),
+      (this[_0x848469(0x100)] = _0x4e0919),
+      (this[_0x848469(0x128)] = _0x41f627),
+      (this[_0x848469(0x132)] = _0x18e227),
+      (this["fakeHost"] = fakeHost),
+      (this["debug"] = debug),
+      (this[_0x848469(0x12d)] = null),
+      (this["brandMessage"] = ""),
+      (this[_0x848469(0x129)] = ""),
+      (this[_0x848469(0x117)] = "");
+  }
+  [_0x4ecc08(0x105)]() {
+    const _0xa1cccf = _0x4ecc08;
+    try {
+      (this[_0xa1cccf(0x12d)] = mc[_0xa1cccf(0x12e)]({
+        host: this["host"],
+        port: this["port"],
+        username: this[_0xa1cccf(0x128)],
+        version: this["version"],
+        fakeHost: this[_0xa1cccf(0x10a)],
+        hideErrors: !![],
+      })),
+        this[_0xa1cccf(0x109)]();
+    } catch (_0x51c949) {
+      new BotResponse(_0xa1cccf(0x107), "", "")["showCliResponse"]();
+    }
+  }
+  [_0x4ecc08(0x131)](_0x1a16d3) {
+    const _0x545c70 = _0x4ecc08;
+    this[_0x545c70(0x134)] === !![] &&
+      console[_0x545c70(0x103)](_0x545c70(0xf5), _0x1a16d3["message"]);
+    if (_0x1a16d3["message"][_0x545c70(0xf7)](_0x545c70(0x125))) {
+      const _0x3152dc =
+        _0x1a16d3[_0x545c70(0x133)]["match"](/\d+\.\d+\.\d+/)[0x0];
+      new BotResponse(
+        "&7Please\x20use\x20version\x20" +
+          _0x3152dc +
+          "\x20to\x20enter\x20the\x20server.",
+        "",
+        ""
+      )[_0x545c70(0x112)]();
+    }
+    if (_0x1a16d3[_0x545c70(0x133)][_0x545c70(0xf7)](_0x545c70(0x102))) {
+      const _0x2d48d9 =
+        _0x1a16d3[_0x545c70(0x133)][_0x545c70(0x114)](/\d+\.\d+\.\d+/)[0x0];
+      new BotResponse(_0x545c70(0x11c) + _0x2d48d9 + _0x545c70(0x11a), "", "")[
+        _0x545c70(0x112)
+      ]();
+    }
+  }
+  [_0x4ecc08(0x109)]() {
+    const _0x33479e = _0x4ecc08;
+    this[_0x33479e(0x12d)]["on"](
+      "error",
+      this["_handleError"][_0x33479e(0x136)](this)
+    ),
+      this[_0x33479e(0x12d)]["on"](
+        _0x33479e(0x11b),
+        this[_0x33479e(0xfb)][_0x33479e(0x136)](this)
+      ),
+      this[_0x33479e(0x12d)]["on"](
+        _0x33479e(0x118),
+        this[_0x33479e(0xfb)][_0x33479e(0x136)](this)
+      ),
+      this["client"]["on"](
+        "custom_payload",
+        this["_handleCustomPayload"][_0x33479e(0x136)](this)
+      ),
+      this[_0x33479e(0x12d)]["on"](
+        _0x33479e(0x119),
+        this[_0x33479e(0x115)][_0x33479e(0x136)](this)
+      ),
+      this[_0x33479e(0x12d)]["on"](
+        "end",
+        this["_handleEnd"][_0x33479e(0x136)](this)
+      );
+  }
+  [_0x4ecc08(0xfb)]() {
+    setTimeout(() => {
+      const _0x132a2c = _0x2e06;
+      new BotResponse(
+        RECONNECT_WITH_BUNGEEHACK
+          ? "&dConnected\x20with\x20bungeehack"
+          : "&aConnected",
+        this[_0x132a2c(0x104)],
+        this[_0x132a2c(0x129)]
+      )[_0x132a2c(0x112)]();
+    }, 0xbb8);
+  }
+  [_0x4ecc08(0x135)](_0x4fb2c1) {
+    const _0x489886 = _0x4ecc08;
+    _0x4fb2c1["channel"] === _0x489886(0x11e) &&
+      (this[_0x489886(0x104)] += _0x4fb2c1[_0x489886(0x10d)][_0x489886(0x127)](
+        _0x489886(0x138)
+      )),
+      _0x4fb2c1[_0x489886(0xff)] === _0x489886(0x106) &&
+        (this[_0x489886(0x129)] += _0x4fb2c1[_0x489886(0x10d)]["toString"](
+          _0x489886(0x138)
+        ));
+  }
+  ["_handleDisconnect"](_0x4fd991) {
+    const _0x59b059 = _0x4ecc08,
+      _0xc4cf2 = getTextFromJSON(_0x4fd991["reason"]);
+    if (
+      _0xc4cf2[_0x59b059(0xf7)](_0x59b059(0x116)) ||
+      RECONNECT_WITH_BUNGEEHACK === ![]
+    ) {
+      this[_0x59b059(0x12d)]["end"](_0x59b059(0x101)),
+        (RECONNECT_WITH_BUNGEEHACK = !![]),
+        setTimeout(() => {
+          const _0x5428f4 = _0x59b059;
+          new BotResponse(_0x5428f4(0x11f), "", "")[_0x5428f4(0x112)]();
+        }, 0x1388),
+        new MinecraftClient({
+          host: host,
+          port: port,
+          username: username,
+          version: version,
+          fakeHost: this[_0x59b059(0x10e)] + _0x59b059(0x122),
+        })[_0x59b059(0x105)]();
+      return;
+    }
+    new BotResponse(
+      _0xc4cf2[_0x59b059(0x13a)](/\s+/g, "\x20")[_0x59b059(0x13a)](
+        /^\s+|\s+$/g,
+        ""
+      ),
+      "",
+      ""
+    )["showCliResponse"]();
+  }
+  [_0x4ecc08(0x10b)](_0x492425) {}
+}
+setTimeout(() => {
+  const _0x17e096 = _0x4ecc08;
+  RECONNECT_WITH_BUNGEEHACK === ![] &&
+    new BotResponse(_0x17e096(0xfa), "", "")[_0x17e096(0x112)]();
+}, 0x1388),
+  new MinecraftClient({
+    host: host,
+    port: port,
+    username: username,
+    version: version,
+  })["connect"]();
